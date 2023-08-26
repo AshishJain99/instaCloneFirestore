c0a860455862f0b97a1be0465b2d274fef3b6984
Ashish Jain <imjainashish@gmail.com>
2023-08-26 23:09:22 +0530

    Initial Commit

---
+++ instaCloneFirestore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instaCloneFirestore
+//
+//  Created by Ashish Jain on 26/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
